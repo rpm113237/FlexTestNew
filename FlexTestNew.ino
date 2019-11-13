1082a2eea7b908d5402563dd5c6a0265e2c54b5c
rpm <rich@highlandcellars.com>
2019-11-13 09:55:03 -0800

    Started Live Test; Think this to be OK

--- FlexTestNew.ino
+++ FlexTestNew.ino
@@ -1,10 +1,10 @@
 //This is a restart to attempt to make the code make sense.  Get rid of the 10X repetitious if's
 #include <EEPROMex.h>
 // define Solenoids
-#define Sol_0_Taut 4
-#define Sol_1_Taut 5
-#define Sol_0_Loose 6
-#define Sol_1_Loose 7
+#define Sol_0_Taut 5
+#define Sol_1_Taut 7
+#define Sol_0_Flex 4
+#define Sol_1_Flex 6
 #define SolON HIGH
 #define SolOFF LOW
 // Define Sensors
@@ -22,15 +22,15 @@ long int CycleCnt = 0;
 #define numcables 2
 char OutString[150];   //for sprintf
 #define CYCLEMS 1000
-#define LOOSEMS 25
-#define TAUTMS  25
-#define SLACKMS (CYCLEMS - LOOSEMS - TAUTMS)/2
+#define FlexMS 50
+#define TAUTMS  50
+#define SLACKMS (CYCLEMS/2 - FlexMS - TAUTMS)
 
-#define STORECNT 100   //store the structure modulo this
+#define STORECNT 500   //store the structure modulo this
 //#define SLACKMS 500
 
 
-#define LOOSE false
+#define Flex false
 #define TAUT true
 
 #define CR Serial.print("\n")
@@ -40,7 +40,7 @@ char OutString[150];   //for sprintf
 struct tracestruct {
   bool trintact;      //true if intact
   bool trFailTaut;    //true if failed taut
-  bool trFailLoose;   //true if failed loose
+  bool trFailFlex;   //true if failed Flex
   long int FirstFail; //Count at which first failed
   long int LastGood;  //Count at which last good
   int  numGoods;       //number of times good since first fail
@@ -51,7 +51,7 @@ struct tracestruct traces;
 struct cablestruct {
   tracestruct traces[numtraces];
   int SolTautPin;     // solonoid to pull tight
-  int SolLoosePin;    //solonoid to push loose
+  int SolFlexPin;    //solonoid to push Flex
   int DrvPin;         //pin to drive low or make input.
   char traceID[numtraces][15];     //such as "48V 8mm", "48V abc" etc.
   int pinSns[numtraces] = {Sns48V, SnsRED, SnsIR, Sns5V, SnsGND};
@@ -76,7 +76,7 @@ void InitStructs() {
 
   tstCnt = EEPROM.readLong(addrCycleCnt);
   if (tstCnt > 0) { //Has been running?
-    Serial.print ("Stored Count at start up = "); Serial.println(tstCnt);
+    Serial.print ("Stored Count at start up = "); Serial.println(tstCnt);CR;CR;
     CycleCnt = tstCnt;
     getStruct();
     reportResults(true);
@@ -101,8 +101,8 @@ void InitStructs() {
     strcpy( cablex[1].traceID[3], "5V_1");
     strcpy( cablex[1].traceID[4], "GND_1");
 
-    cablex[0].SolLoosePin = Sol_0_Loose;
-    cablex[1].SolLoosePin = Sol_1_Loose;
+    cablex[0].SolFlexPin = Sol_0_Flex;
+    cablex[1].SolFlexPin = Sol_1_Flex;
     cablex[0].SolTautPin = Sol_0_Taut;
     cablex[1].SolTautPin = Sol_1_Taut;
 
@@ -113,7 +113,7 @@ void InitStructs() {
       for (tr = 0; tr < numtraces; tr++) {  //then traces
         cablex[cs].traces[tr].trintact = true;
         cablex[cs].traces[tr].trFailTaut = false;
-        cablex[cs].traces[tr].trFailLoose = false;
+        cablex[cs].traces[tr].trFailFlex = false;
         //        cablex[cs].traces[tr].FirstFail = 12345678 + tr+ (cs+1)*10;
         cablex[cs].traces[tr].FirstFail = 0;
         cablex[cs].traces[tr].LastGood = 0;
@@ -129,14 +129,11 @@ void InitStructs() {
 }
 
 void writeStruc(void) {
-  //  bool verbose = true;
+  
   //    Serial.print("\n************Store Struc at Cnt =" ); Serial.println(CycleCnt);
   EEPROM.updateBlock(addStruc0, cablex[0]);
   EEPROM.updateBlock(addStruc1, cablex[1]);
-  //    Serial.println("************Readback Stored Struc at Cnt =" );
-  //    getStruct();
-  //    reportResults(verbose);
-  //    Serial.println("******************************************\n" );
+  
 
 
 }
@@ -149,19 +146,19 @@ void reportResults(bool verbose) {
   ltoa(CycleCnt, cntstr, 10);
   //Serial.print ("At CycleCnt = "); Serial.println(cntstr);
 
-  if (verbose) {    //print all the stuff
-    Serial.println("\n**************In reportResults (verbose)***********");
-    for (cs = 0; cs < numcables; cs++) {
-      Serial.print ("LoosePin = "); Serial.print(cablex[cs].SolLoosePin);
-      Serial.print (" TautPin = "); Serial.print(cablex[cs].SolTautPin); CR;
-    }   CR;
-  }
+//  if (verbose) {    //print all the stuff
+//    Serial.println("\n**************In reportResults (verbose)***********");
+//    for (cs = 0; cs < numcables; cs++) {
+//      Serial.print ("FlexPin = "); Serial.print(cablex[cs].SolFlexPin);
+//      Serial.print (" TautPin = "); Serial.print(cablex[cs].SolTautPin); CR;
+//    }   CR;
+//  }
 
+
+  Serial.print ("**********START Report at CycleCnt = "); Serial.print(cntstr);Serial.println ("**********");
   for (cs = 0; cs < numcables; cs++) {
     ltoa(CycleCnt, cntstr, 10);
-    Serial.print(".......Side = "); Serial.print(cs); Serial.print (" CycleCnt = "); Serial.print(cntstr);
-    Serial.println(" ................");
-    //  for (i = 0; i<numtraces; i++){Serial.print(cablex[cs].traceID[i]); TAB;} CR;
+    Serial.print("Side = "); Serial.println(cs); 
     for (tr = 0; tr < numtraces; tr++) {
       Serial.print(cablex[cs].traceID[tr]); TAB;
       if (cablex[cs].traces[tr].trintact == true)Serial.print("GOOD\t");
@@ -169,7 +166,7 @@ void reportResults(bool verbose) {
         ltoa(cablex[cs].traces[tr].FirstFail, cntstr, 10); //real
         Serial.print ("OPEN @ "); Serial.print(cntstr); TAB;
         if (cablex[cs].traces[tr].trFailTaut)Serial.print("Fail: Taut\t");
-        else if (cablex[cs].traces[tr].trFailLoose)Serial.print("Fail: Loose\t");
+        else if (cablex[cs].traces[tr].trFailFlex)Serial.print("Fail: Flex\t");
       }
       if (cablex[cs].traces[tr].LastGood >= 0) {
         ltoa(cablex[cs].traces[tr].LastGood, cntstr, 10); //real
@@ -182,8 +179,8 @@ void reportResults(bool verbose) {
 
     CR;
   }//end for cs
-
-  CR;
+  ltoa(CycleCnt, cntstr, 10);
+  Serial.print ("**********END Report at CycleCnt = "); Serial.print(cntstr);Serial.println ("**********\n");
 
 
 }//end report
@@ -196,14 +193,14 @@ void setup() {
   long int Elong = 0;
   bool verbose = true;
   Serial.begin(115200);
-  Serial.println("\nStarting.......");
+  Serial.println("\nStarting Serial.......");
 
   EEPROM.setMaxAllowedWrites (10000);    //default is 100
   EEPROM.setMemPool(0, EEPROMSizeUno);
 
   //  while(1);
   //  uncomment the following to reset EEPROM
-  //  ZeroEEPROM();
+//    ZeroEEPROM();
   //uncomment to print out EEPROM addresses
   //  PrintEEPROMAddresses();
   InitStructs();
@@ -212,8 +209,8 @@ void setup() {
   for (i = 0; i < numcables; i++) { //cleaner in initstructs???
     pinMode(cablex[i].SolTautPin, OUTPUT);
     //    Serial.print(cablex[i].SolTautPin); SP; Serial.print(i); SP; Serial.println(" Taut Pin At Setup");
-    pinMode(cablex[i].SolLoosePin, OUTPUT);
-    //    Serial.print(cablex[i].SolLoosePin);  SP; Serial.print(i); SP; Serial.println(" Loose Pin At Setup");
+    pinMode(cablex[i].SolFlexPin, OUTPUT);
+    //    Serial.print(cablex[i].SolFlexPin);  SP; Serial.print(i); SP; Serial.println(" Flex Pin At Setup");
   }
   //  Serial.println("After set solenoids--silly to print here");
   //  reportResults(verbose);    //deubug
@@ -222,24 +219,27 @@ void setup() {
 void solenoidsSet(int side, bool istaut) {
   int i = 0;
   // if istaut, fire taut solenoids for int side
-  //if !istaut, fire loose solenoids for int side
+  //if !istaut, fire Flex solenoids for int side
   // turn all solenoids OFF then (side) ON
   //if side <0; means leave all off.
-
-
-
+  
   if (side >= 0) {
-    //    Serial.print("For Side = "); Serial.println(side);
-    if (istaut)digitalWrite(cablex[side].SolTautPin, SolON);
-    //    Serial.print("setting TAUT ON, pin = "); Serial.println(cablex[side].SolTautPin);}
-    if (!istaut)digitalWrite(cablex[side].SolLoosePin, SolON);
-    //    Serial.print("setting LOOSE ON, pin = "); Serial.println(cablex[side].SolLoosePin);}
+//        Serial.print("For Side = "); Serial.println(side);
+    if (istaut){
+      digitalWrite(cablex[side].SolTautPin, SolON);
+//      Serial.print("setting TAUT ON, pin = "); Serial.println(cablex[side].SolTautPin);
+      }
+      
+    if (!istaut){
+      digitalWrite(cablex[side].SolFlexPin, SolON);   
+//      Serial.print("setting Flex ON, pin = "); Serial.println(cablex[side].SolFlexPin);
+      }
   }
   else {
     //    Serial.print("Slack");
     for (i = 0; i < numcables; i++) {
       digitalWrite(cablex[i].SolTautPin, SolOFF);
-      digitalWrite(cablex[i].SolLoosePin, SolOFF);
+      digitalWrite(cablex[i].SolFlexPin, SolOFF);
     }
 
   }
@@ -249,14 +249,15 @@ void solenoidsSet(int side, bool istaut) {
 
 void setSide(int side) {
   //Init side for test
-  //
   int cs = 0;
+    
   for (cs = 0; cs < numcables; cs++) pinMode(cablex[cs].DrvPin, INPUT_PULLUP);
-  //make everthing an input
+  //make everything an input
   pinMode(cablex[side].DrvPin, OUTPUT);
+//  Serial.print("set Drive pin Low"); SP; Serial.print(side); SP; Serial.println(cablex[side].DrvPin);
   digitalWrite(cablex[side].DrvPin, LOW);
-
-}
+  delay(1);
+  }
 
 
 void cktraces(bool taut) {
@@ -275,12 +276,12 @@ void cktraces(bool taut) {
       delay(5); //give it 5 ms to settle.               //move to setup???
 
       if (digitalRead(cablex[cs].pinSns[tnm]) == HIGH) { //floated HIGH==bad
-        //          Serial.print("cktraces finds high for cable ="); Serial.println(tnm);
+            Serial.print("cktraces finds high for cable ="); Serial.println(tnm);
         if (cablex[cs].traces[tnm].trintact == true) {
-          //cablex[cs].traces[tnm].FirstFail = CycleCnt;//!!!!!for debug
+          cablex[cs].traces[tnm].FirstFail = CycleCnt;
           cablex[cs].traces[tnm].trintact = false;
           if (taut == true)cablex[cs].traces[tnm].trFailTaut = true;
-          else cablex[cs].traces[tnm].trFailLoose = true;
+          else cablex[cs].traces[tnm].trFailFlex = true;
           //Serial.print ("Do we get end of digital read high? "); Serial.println(cs);
         }// end if == true
       }// end if High
@@ -292,9 +293,10 @@ void cktraces(bool taut) {
 
       }//end else
 
-    }//end for
-
-  }
+    }//end for tnm
+  
+  }//end for cs
+  pinMode(cablex[cs].DrvPin, INPUT_PULLUP); //float it
 
 }//end cktraces
 
@@ -317,8 +319,8 @@ void loop() {
 
   //first, do taut
   CycleCnt++;
-  //  Serial.print("CycleCnt (MAIN LOOP) = "); Serial.println (CycleCnt);
   istaut = true;
+
   for (i = 0; i < numcables; i++) solenoidsSet(i, istaut);  //set all taut
   delay (TAUTMS);
   cktraces(istaut);
@@ -327,7 +329,7 @@ void loop() {
 
   istaut = false;
   for (i = 0; i < numcables; i++) solenoidsSet(i, istaut);
-  delay (LOOSEMS);
+  delay (FlexMS);
   cktraces(istaut);
   solenoidsSet(-1, istaut);
   delay(SLACKMS);
@@ -341,8 +343,8 @@ void loop() {
 //    Serial.println("in loop readback = ");
     reportResults(true);
     Serial.println("-----------------------------------");
-    Serial.print(" CycleCnt written out at CycleCnt = "); Serial.println(CycleCnt);
-    Serial.println("-----------------------------------");
+    Serial.print(" Data Stored at CycleCnt = "); Serial.println(CycleCnt);
+    Serial.println("-----------------------------------\n");
 
   }
   //    }   //end debug
